aaeb1eb4ac9362bdb29a2673981bbb3c37a8b592
rti2 <149401693+rti2@users.noreply.github.com>
2024-12-18 19:06:50 +0330

    Redirect YT to embed YT

--- js/background.js
+++ js/background.js
@@ -1,28 +1,21 @@
 window.browser = window.browser || window.chrome;
 
-const INVIDIOUS_INSTANCES = [];
-
-fetch("https://api.invidious.io/instances.json")
-	.then((resp) => resp.json())
-	.then((array) => array.forEach((json) => INVIDIOUS_INSTANCES.push(json[0])));
-
 browser.webRequest.onBeforeRequest.addListener(
-	(details) => {
-		const url = new URL(details.url);
-		if (url.hostname.endsWith("youtu.be") && url.pathname.length > 1) {
-			return { redirectUrl: "https://piped.kavin.rocks/watch?v=" + url.pathname.substr(1) };
-		}
-		if (
-			url.hostname.endsWith("youtube.com") ||
-			url.hostname.endsWith("youtube-nocookie.com") ||
-			INVIDIOUS_INSTANCES.includes(url.hostname)
-		) {
-			url.hostname = "piped.kavin.rocks";
-			return { redirectUrl: url.href };
-		}
-	},
-	{
-		urls: ["<all_urls>"],
-	},
-	["blocking"],
+    (details) => {
+        const url = new URL(details.url);
+        
+        if (url.hostname.endsWith("youtube.com") && url.searchParams.has("v")) {
+            const videoId = url.searchParams.get("v");
+            return { redirectUrl: `https://www.youtube.com/embed/${videoId}` };
+        }
+
+        if (url.hostname.endsWith("youtu.be") && url.pathname.length > 1) {
+            const videoId = url.pathname.substr(1);
+            return { redirectUrl: `https://www.youtube.com/embed/${videoId}` };
+        }
+    },
+    {
+        urls: ["<all_urls>"],
+    },
+    ["blocking"]
 );
